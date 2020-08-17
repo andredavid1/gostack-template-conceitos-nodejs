1373eee8137bdc6ea05febfcf88b1a636f2fdc7b
André David <andredavid1@yahoo.com.br>
2020-08-17 18:54:25 -0300

    post repository

--- src/app.js
+++ src/app.js
@@ -11,12 +11,17 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  const {} = request.body;
   // TODO
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository)
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
